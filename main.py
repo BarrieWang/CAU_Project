db4cb7970ed65fd5ccbcbe8ba9be0cd20778e3a7
Barrie <BarrieCAU@outlook.com>
2020-03-01 09:42:40 +0800

    Update project structure version2.0

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world yy")
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world(example1, example2):
+    """
+    对函数或类的解释说明，要求所有函数、类、其中的关键部分注释要完整
+    :param example1: 对每个输入变量的解释说明
+    :param example2: 对每个输入变量的解释说明
+    :return: 对输出结果的解释说明
+    """
+    return example1 + example2
+
+# 写完后应逐个检查右侧的warning，尽量要求不出现
