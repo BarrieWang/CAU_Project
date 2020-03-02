98091e4309d2ca00f259443ac07edfc10247c97b
Vincent_TAO <wangweitao1998@gmail.com>
2020-03-02 16:05:56 +0800

    构建Flask基本框架
    1. 增加Flask测试页面
    2. 修改.gitignore - 请在本地创建test.py进行代码调试，此文件不会被同步

--- main.py
+++ main.py
@@ -1,15 +1,48 @@
-from flask import Flask
+from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world(example1, example2):
+def index():
     """
-    对函数或类的解释说明，要求所有函数、类、其中的关键部分注释要完整
-    :param example1: 对每个输入变量的解释说明
-    :param example2: 对每个输入变量的解释说明
-    :return: 对输出结果的解释说明
+    首页
+    :return: index.html
     """
-    return example1 + example2
+    pass
+    return render_template("index.html")
+
+
+@app.route('/helloworld')
+def hello_world():
+    """
+    Hello World示例网页
+    :return: helloworld.html
+    """
+    return render_template("helloworld.html")
+
+
+@app.route('/jsonrequest')
+def jsonrequest():
+    """
+    Jquery动态请求示例
+    :return: Json格式请求结果
+    """
+    words = "Hello World!"
+    wjson = json.dumps({"result": words})
+    return wjson
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# def hello_world(example1, example2):
+#     """
+#     对函数或类的解释说明，要求所有函数、类、其中的关键部分注释要完整
+#     :param example1: 对每个输入变量的解释说明
+#     :param example2: 对每个输入变量的解释说明
+#     :return: 对输出结果的解释说明
+#     """
+#     return example1 + example2
 
 # 写完后应逐个检查右侧的warning，尽量要求不出现
